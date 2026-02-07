96211f6d2891ef134fe394549055cf1b9321dbc1
ToKarotik <markzhiv@gmail.com>
2026-02-07 17:43:37 +0200

    fixing

--- scripts/tech/handle_paths.js
+++ scripts/tech/handle_paths.js
@@ -1,28 +1,26 @@
-
 let PATH_404_PAGE = "/pages/tech/404.html";
-
 let PAGES_LIST = [
     "beauty",
     "index-demo"
 ]
 
-function is_in_pages_list(path)
-{
+function is_in_pages_list(path) {
     return PAGES_LIST.includes(path.slice(1));
 }
 
-
-function handle_paths(){
+function handle_paths() {
     let path = location.pathname;
     var to_direct = PATH_404_PAGE;
-
-    if(path == "/"){                  // is index page
+    if(path == "/") {
         to_direct = "/index.html";
     }
-    if(is_in_pages_list(path)){       // is in pages list
+    if(is_in_pages_list(path)) {
         to_direct = path;
     }
     console.log("Path: " + path);
     console.log("Direct to: " + to_direct);
-    return to_direct
+    return to_direct;
 }
+
+// Export the function as default
+export default handle_paths;
